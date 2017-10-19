54d1fa9020d94c4d4394820d59c6aac62f22a631
史守圆 <shishouyuan1994216@live.cn>
2017-10-19 20:26:02 +0800

    0

--- IEC104/ASDU.cs
+++ IEC104/ASDU.cs
@@ -11,6 +11,29 @@ namespace Shouyuan.IEC104
     /// </summary>
     public class ASDU
     {
+        /// <summary>
+        /// 类型标识。
+        /// </summary>
+        public byte Type;
 
+        /// <summary>
+        /// 可变结构限定词。最高位为SQ，其余7位表示信息体的个数。
+        /// </summary>
+        public byte VarConstrain;
+
+        /// <summary>
+        /// 传送原因。2字节，高字节为源发地址。
+        /// </summary>
+        public byte[] COT = new byte[2];
+
+        /// <summary>
+        /// 应用服务数据单元公共地址。2字节，高字节固定为0。
+        /// </summary>
+        public byte addr;
+
+        /// <summary>
+        /// 信息体集合
+        /// </summary>
+        public Message[] Messages;
     }
 }
---
+++ IEC104/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shouyuan.IEC104
+{
+    /// <summary>
+    /// 信息体对象
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// 信息体地址。可选1、2、3个字节。3字节时最高字节一般置0。
+        /// </summary>
+        public byte[] Addr;
+
+        /// <summary>
+        /// 信息体元素。
+        /// </summary>
+        public byte[] Element;
+
+        /// <summary>
+        /// 信息体时标。可选2、3、7个字节。
+        /// </summary>
+        public byte[] TimeStamp;
+    }
+}
