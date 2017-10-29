9dddf8037b5860a25ae907a365fc3151ead1af3a
史守圆 <shishouyuan1994216@live.cn>
2017-10-29 10:27:41 +0800

    s

--- IEC104/Datagram.cs
+++ IEC104/Datagram.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Shouyuan.IEC104
 {
-   abstract class  Datagram
+    public  abstract class  Datagram
     {
-       
+        public readonly APDU APDU=new APDU();
+        public virtual byte Type { get; set; }   
+        public virtual DatagramFormat DatagramFormat { get => DatagramFormat.Unknown; }
+        public virtual void SendTo(System.Net.Sockets.Socket socket)
+        {
+            APDU.SendTo(socket);
+        }
+    }
+
+    public class M_ME_NA_1 : Datagram
+    {
+        
     }
 }
