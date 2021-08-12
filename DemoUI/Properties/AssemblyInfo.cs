57f632c2b8f1159bb4f9181be906756190ae618c
shouyuan <shishouyuan@outlook.com>
2021-08-12 20:03:38 +0800

    version number changed to 0.1.0

--- DemoUI/Properties/AssemblyInfo.cs
+++ DemoUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("GuiZhou104")]
+[assembly: AssemblyTitle("IEC 104")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Shi Shouyuan")]
-[assembly: AssemblyProduct("GuiZhou104")]
+[assembly: AssemblyProduct("IEC 104")]
 [assembly: AssemblyCopyright("Copyright © Shi Shouyuan  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
